0c2e58e6566c843c960798e8da181ba354e8fb5f
RohitSaharan0398 <rohit8607745768@gmail.com>
2025-05-13 09:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter=0;
+console.log(counter)
+let button =document.getElementById("Btn");
+button.addEventListener('click',function (){
+	counter++;
+});
+
